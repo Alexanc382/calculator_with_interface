8d820e1542f09da1166a597c5255a3dc191d30a3
Alexanc382 <alexanc382@gmail.com>
2025-07-26 07:46:22 +0300

    добавила стиль кнопок

--- main.py
+++ main.py
@@ -57,31 +57,36 @@ def validate_entry():
 
 window = Tk()
 window.title('Калькулятор')
+style = ttk.Style()
+style.configure('numbers.TButton', background='#FFC0C0') # цифры
+style.configure('arithmetic_oper.TButton', background='#DDA0DD') # арифм. действия
+style.configure('С.TButton', background='#FF69B4') # кнопка "С"
+style.configure('equals.TButton', background='#FF00FF') # "равно"
+
 entry = ttk.Entry()
 entry.grid(row=0, column=0, columnspan=3, sticky='ew') #растянули на три столбца
 entry.bind('<KeyRelease>',lambda event: validate_entry())
 
-ttk.Button(text='1', command=lambda: enter_number('1')).grid(row=1, column=0)
-ttk.Button(text='2', command=lambda: enter_number('2')).grid(row=1, column=1)
-ttk.Button(text='3', command=lambda: enter_number('3')).grid(row=1, column=2)
-ttk.Button(text='4', command=lambda: enter_number('4')).grid(row=2, column=0)
-ttk.Button(text='5', command=lambda: enter_number('5')).grid(row=2, column=1)
-ttk.Button(text='6', command=lambda: enter_number('6')).grid(row=2, column=2)
-ttk.Button(text='7', command=lambda: enter_number('7')).grid(row=3, column=0)
-ttk.Button(text='8', command=lambda: enter_number('8')).grid(row=3, column=1)
-ttk.Button(text='9', command=lambda: enter_number('9')).grid(row=3, column=2)
-ttk.Button(text='0', command=lambda: enter_number('0')).grid(row=4, column=0)
+ttk.Button(text='1', command=lambda: enter_number('1'), style='numbers.TButton').grid(row=1, column=0)
+ttk.Button(text='2', command=lambda: enter_number('2'), style='numbers.TButton').grid(row=1, column=1)
+ttk.Button(text='3', command=lambda: enter_number('3'), style='numbers.TButton').grid(row=1, column=2)
+ttk.Button(text='4', command=lambda: enter_number('4'), style='numbers.TButton').grid(row=2, column=0)
+ttk.Button(text='5', command=lambda: enter_number('5'), style='numbers.TButton').grid(row=2, column=1)
+ttk.Button(text='6', command=lambda: enter_number('6'), style='numbers.TButton').grid(row=2, column=2)
+ttk.Button(text='7', command=lambda: enter_number('7'), style='numbers.TButton').grid(row=3, column=0)
+ttk.Button(text='8', command=lambda: enter_number('8'), style='numbers.TButton').grid(row=3, column=1)
+ttk.Button(text='9', command=lambda: enter_number('9'), style='numbers.TButton').grid(row=3, column=2)
+ttk.Button(text='0', command=lambda: enter_number('0'), style='numbers.TButton').grid(row=4, column=0)
 
-ttk.Button(text='+', command=lambda: set_operation('+')).grid(row=4, column=1)
-ttk.Button(text='-', command=lambda: set_operation('-')).grid(row=4, column=2)
-ttk.Button(text='*', command=lambda: set_operation('*')).grid(row=5, column=0)
-ttk.Button(text='/', command=lambda: set_operation('/')).grid(row=5, column=1)
+ttk.Button(text='+', command=lambda: set_operation('+'), style='arithmetic_oper.TButton').grid(row=4, column=1)
+ttk.Button(text='-', command=lambda: set_operation('-'), style='arithmetic_oper.TButton').grid(row=4, column=2)
+ttk.Button(text='*', command=lambda: set_operation('*'), style='arithmetic_oper.TButton').grid(row=5, column=0)
+ttk.Button(text='/', command=lambda: set_operation('/'), style='arithmetic_oper.TButton').grid(row=5, column=1)
+ttk.Button(text='x²', command=lambda: set_exponen_2('first'), style='arithmetic_oper.TButton').grid(row=5, column=2)
+ttk.Button(text='x³', command=lambda: set_exponen_3('first'), style='arithmetic_oper.TButton').grid(row=6, column=0)
 
-ttk.Button(text='x²', command=lambda: set_exponen_2('first')).grid(row=5, column=2)
-ttk.Button(text='x³', command=lambda: set_exponen_3('first')).grid(row=6, column=0)
-
-ttk.Button(text='C', command=clear_entry).grid(row=6, column=1)
-ttk.Button(text='=', command=calc).grid(row=6, column=2)
+ttk.Button(text='C', command=clear_entry, style='С.TButton').grid(row=6, column=1)
+ttk.Button(text='=', command=calc, style='equals.TButton').grid(row=6, column=2)
 
 
 
